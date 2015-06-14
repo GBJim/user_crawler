f16ae324a2b3d91a9eb7acb5b29f50d244ac3d14
gb <ccha97u@gmail.com>
2015-06-14 16:14:01 +0800

    I don't know what I did

--- idCrawler.py
+++ idCrawler.py
@@ -11,19 +11,21 @@ def getTweets(screen_name):
 	access_token_secret ="kLYgBTPeslLgaFugCx0PoiBpPIKnyCBEVfqqJCkjsSKpP"
 	twitter = Twython(consumer_key, consumer_secret,access_token,access_token_secret )
 
-	tweets = twitter.get_user_timeline(screen_name = screen_name, count = 200, trim_user = True, include_rts = False  )
+	tweets = twitter.get_user_timeline(screen_name = screen_name, count = 200, trim_user = False, include_rts = True  )
 
 	totalTweets = tweets
 	while len(tweets) >= 2:
 		max_id = tweets[-1]["id"]
-		tweets = twitter.get_user_timeline(screen_name = screen_name, max_id = max_id, count = 200, trim_user = True, include_rts = False)
+		tweets = twitter.get_user_timeline(screen_name = screen_name, max_id = max_id, count = 200, trim_user = False, include_rts = True)
 		if len(tweets) > 1:
 			totalTweets += tweets[1:]
 	return totalTweets
 
 
+
+'''
 #collection = pymongo.MongoClient().idea.BDP_tweets
-candidates = []  #fill in the ids you need to crawl
+candidates = ["nyse", "DowJones", "BSEIndia"]  #fill in the ids you need to crawl
 #candidates = json.load(open("BDP_candidates.json"))
 
 tweets = []
@@ -32,5 +34,10 @@ for screen_name in candidates:
 	tweets += getTweets(screen_name)
 	print(screen_name + "is done!")
 
+w = open("Ashcok.json", "w")
+json.dump(tweets, w)
+w.close()
+
 #collection.insert(tweets)
 print("Insertion complete")
+'''
